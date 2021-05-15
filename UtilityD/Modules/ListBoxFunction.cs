e691fe9244d6b7b37bd14598e60afb519a905219
Shade <73481203+Shade0122@users.noreply.github.com>
2021-05-15 19:02:22 +0100

    Add files via upload

---
+++ UtilityD/Modules/ListBoxFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Windows.Controls;
+
+namespace UtilityD.Modules
+{
+    class ListBoxFunction
+    {
+        public static void ListBoxRefresh(ListBox listBox, string Folder, string FileType)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Folder);
+            FileInfo[] files = directoryInfo.GetFiles(FileType);
+            foreach (FileInfo fileInfo in files)
+            {
+                listBox.Items.Clear();
+                listBox.Items.Add(fileInfo.Name);
+            }
+        }
+    }
+}
